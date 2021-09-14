41d0be975d7d6d11322b5d153789c32d1d329c86
Benjamin Patton <benjaminpatton@Benjamins-MacBook-Pro.local>
2021-09-13 22:42:26 -0400

    updated vercel deployments

--- app/routes/rating.jsx
+++ app/routes/rating.jsx
@@ -10,78 +10,79 @@ export function links() {
 export async function loader() {
   let items = await prisma.item.findMany({
     include: {
-      Review: true
-    }
+      Review: true,
+    },
   });
-  
+
   // use the review
   return json(items, {
     Headers: {
-      "Cache-Control": "max-age=60", 
-    }
-  }); 
+      "Cache-Control": "max-age=60",
+    },
+  });
 }
 
 export async function action({ request }) {
   const req = request.clone();
-  let body = new URLSearchParams(await req.text())
-  console.log(body)
+  let body = new URLSearchParams(await req.text());
+  console.log(body);
   await prisma.item.create({
     data: {
       itemTitle: body.get("title"),
-      itemDescription: body.get("item_description")
-    }
-  })
-  return redirect("/rating")
+      itemDescription: body.get("item_description"),
+    },
+  });
+  return redirect("/rating");
 }
 
 export default function Rating() {
-  let data = useRouteData(); 
+  let data = useRouteData();
 
   return (
-      <div className="box">
-        
-          <h2>Top Rated Items!</h2> 
-          <div className="ratingsContainer">
-          {data.map((item) => (
-            <div className="itemContainer">
-              <div>
-                <NavLink to={item.id} key={item.id}>{item.itemTitle}</NavLink>
-              </div>
-              <div>
-                <p>{item.itemDescription}</p>
-              </div>
+    <div className="box">
+      <h2>Top Rated Items!</h2>
+      <div className="ratingsContainer">
+        {data.map((item) => (
+          <div className="itemContainer">
+            <div>
+              <NavLink to={item.id} key={item.id}>
+                {item.itemTitle}
+              </NavLink>
             </div>
-          ))}
+            <div>
+              <p>{item.itemDescription}</p>
+            </div>
           </div>
-          <div>
-            <form method="post" action="/rating">
-              <div className="Item_Form">
-                <h2>Add a Item To Rate</h2>
-                <input 
-                  type="text"
-                  placeholder="Item Title"
-                  aria-label="Item Title"
-                  name="title"
-                /> 
-                <textarea 
-                  type="text"
-                  placeholder="Describe the Item"
-                  aria-label="Describe the Item"
-                  name="item_description"
-                /> 
-                <div className="Form_Button">
-                  <button type="submit">Add Item</button>
-                </div>
-              </div>
-            </form>
-        </div>
-        </div>
-  )
-
+        ))}
+      </div>
+      <div>
+        <form method="post" action="/rating">
+          <div className="Item_Form">
+            <h2>Add a Item To Rate</h2>
+            <input
+              type="text"
+              placeholder="Item Title"
+              aria-label="Item Title"
+              name="title"
+            />
+            <textarea
+              type="text"
+              placeholder="Describe the Item"
+              aria-label="Describe the Item"
+              name="item_description"
+            />
+            <div className="Form_Button">
+              <button type="submit">Add Item</button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
 
-{/* <p>Rating component</p>
+{
+  /* <p>Rating component</p>
             <div className="starContainer">
             {[1, 2, 3, 4, 5].map((index) => (
               <RatingIcon
@@ -95,11 +96,5 @@ export default function Rating() {
                 />
             ))}
             </div>
-            <p>Rating - {rating}</p> */}
-
-
-
-
-
-
-
+            <p>Rating - {rating}</p> */
+}
